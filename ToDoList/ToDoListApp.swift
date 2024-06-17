ec2c4bfd0d494ba804f2fe2ffdd4a7ead02c4a32
Khanh Le <masterbruculous@gmail.com>
2024-06-17 23:25:29 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by MasterBi on 17/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
