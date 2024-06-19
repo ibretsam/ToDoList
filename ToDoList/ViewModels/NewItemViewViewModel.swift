95ab73782bc87589f19ee27c036608da09efd380
Khanh Le <masterbruculous@gmail.com>
2024-06-19 22:49:30 +0700

    Save To do list item to Firebase db

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by MasterBi on 17/6/24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -14,7 +16,33 @@ class NewItemViewViewModel: ObservableObject {
     init () {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        // Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        // Save model to db
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
