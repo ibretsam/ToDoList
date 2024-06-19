97f7abba6440acfc3603e180c1fc7ca6ac38cf86
Khanh Le <masterbruculous@gmail.com>
2024-06-19 23:34:06 +0700

    Implement delete item

--- ToDoList/ViewModels/ToDoListViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -5,14 +5,27 @@
 //  Created by MasterBi on 17/6/24.
 //
 
+import FirebaseFirestore
 import Foundation
 
 class ToDoListViewViewModel: ObservableObject {
     @Published var showingNewItemView = false
     
-    init () {}
+    private let userId: String
     
+    init (userId: String) {
+        self.userId = userId
+    }
+    
+    /// Delete to do list item
+    /// - Parameter id: item id to delete
     func delete(id: String) {
+        let db = Firestore.firestore()
         
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
     }
 }
